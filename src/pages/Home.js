2ff0e94bb503e5e245aa9efd69e9e10a1503d14f
sopoour <sophia.auer@googlemail.com>
2021-12-07 16:12:10 +0100

    remove Section wrapper from Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,38 +9,47 @@ import { Link } from "react-router-dom";
 const Home = (props) => {
   const [isActive, setIsActive] = useState(false);
 
-  const { searchResults, searchTerm, handleChange } = useSearchData(props.data, true, setIsActive);
-
+  const { searchResults, searchTerm, handleChange } = useSearchData(
+    props.data,
+    true,
+    setIsActive
+  );
 
   const handleBlur = () => {
-      setIsActive(false)
-  }
+    setIsActive(false);
+  };
 
   return (
-    <Section>
-      <SearchBar
-        searchTerm={searchTerm}
-        onChange={handleChange}
-        onKeyUp={handleChange}
-        onBlur={handleBlur}
-      />
-      {isActive && (
-        <CardContainer
-          isLoading={props.isLoading}
-          httpError={props.httpError}
-          data={searchResults}
+    <React.Fragment>
+      <Section>
+        <SearchBar
+          searchTerm={searchTerm}
+          onChange={handleChange}
+          onKeyUp={handleChange}
+          onBlur={handleBlur}
         />
-      )}
+        {isActive && (
+          <CardContainer
+            isLoading={props.isLoading}
+            httpError={props.httpError}
+            data={searchResults}
+          />
+        )}
+      </Section>
 
       {!isActive && (
         <React.Fragment>
-          <Link to="/eolt/contents" className="btnDark centered" style={{width: "100px", height: "20px"}}>
+          <Link
+            to="/eolt/contents"
+            className="btnDark centered"
+            style={{ width: "100px", height: "20px" }}
+          >
             Browse
           </Link>
           <Info />
         </React.Fragment>
       )}
-    </Section>
+    </React.Fragment>
   );
 };
 
